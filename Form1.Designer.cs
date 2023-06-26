ae1ccfe559f0eb3513541a13411df0aaef3b640c
ay2048 <55039025+ahayat2806@users.noreply.github.com>
2023-06-27 00:49:31 +0100

    Upload
    Uploaded code

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace KitCrypt_Keygen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.keyOut = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.keyIn = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // keyOut
+            // 
+            this.keyOut.Location = new System.Drawing.Point(12, 64);
+            this.keyOut.Name = "keyOut";
+            this.keyOut.Size = new System.Drawing.Size(776, 20);
+            this.keyOut.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Encrypted String (For Containment)";
+            // 
+            // keyIn
+            // 
+            this.keyIn.Location = new System.Drawing.Point(12, 25);
+            this.keyIn.Name = "keyIn";
+            this.keyIn.Size = new System.Drawing.Size(776, 20);
+            this.keyIn.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Decrypted String (For Usage)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 117);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.keyIn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.keyOut);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "KitCrypt Keygen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox keyOut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox keyIn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace KitCrypt_Keygen
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string sha256(string randomString)
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new System.Text.StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(randomString));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            keyIn.Text = "";
+            keyOut.Text = "";
+
+            // Get the number of words and letters per word.
+            int num_letters = 16;
+            int num_words = 4;
+
+            // Make an array of the letters we will use.
+            char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890".ToCharArray();
+
+            // Make a random number generator.
+            Random rand = new Random();
+
+            // Make the words.
+            for (int i = 1; i <= num_words; i++)
+            {
+                // Make a word.
+                string word = "";
+                for (int j = 1; j <= num_letters; j++)
+                {
+                    // Pick a random number between 0 and 25
+                    // to select a letter from the letters array.
+                    int letter_num = rand.Next(0, letters.Length - 1);
+
+                    // Append the letter.
+                    word += letters[letter_num];
+                }
+
+                // Add the word to the list.
+                keyIn.Text = (word);
+                keyOut.Text = sha256(word);
+            }
+        }
+    }
+}
